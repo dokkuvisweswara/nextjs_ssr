e8b58a8c8152b4398c9fd308b80045bf530998ab
dokkuvisweswara <visweswara.dokku@gmail.com>
2022-12-02 12:18:35 +0530

    ok ok

--- pages/Carousel/index.js
+++ pages/Carousel/index.js
@@ -3,10 +3,8 @@ import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 import { useRouter } from 'next/router'
 
-export default function Carousel (props) {
-    const [mount, setMount] = useState(false);
-    const [carousalData, setCarousalData] = useState(props.data);
-    const [iterableData, setIterableData] = useState([]);
+export default function Carousel ({data}) {
+    const [carousalData, setCarousalData] = useState([]);
     const router = useRouter()
     
     const handleClick = async (u) => {
@@ -14,28 +12,20 @@ export default function Carousel (props) {
         alert(u)
     }
     
-    useEffect(() => {
-        if(!mount){
-            console.log("---", carousalData)
-            setMount(true);
-            inItSetTimeOut();
-        }
-        // eslint-disable-next-line
-    }, []);
-    async function inItSetTimeOut() {
-        setTimeout(() => {
-            const x = [...carousalData];
-            x.push(x[1]);
-            setCarousalData([...x]);
-        }, 100)
-    }
+    // useEffect(() => {
+    //     console.log("{}", data);
+    //     // const url = "https://catalogue-ms.cloud.altbalaji.com/v1/v1/section/list/S1_Lock_Upp-Preview_Carousal?domain=IN&limit=10";
+    //     // fetchCarousel(url).then((x)=>{
+    //     //     console.log("....", x);
+    //     //     setCarousalData(...x)});
+    // }, [])
     return (
         <>
          <div className={styles.carousel}>
         {
-        carousalData && carousalData.length> 0 ? carousalData.map((item, index) => (
-            <button onClick={() => {handleClick(item.title)}} key={index}>
-                <Image unoptimized src={item.images[0].url} alt={item.title} width={100} height={200} key={index} className={styles.carouselImag}/>
+        data && data.length> 0 ? data.map((x, index) => (
+            <button onClick={() => {handleClick(x.title)}} key={index}>
+                <Image unoptimized src={x.images[0].url} alt={x.title} width={100} height={200} key={index} className={styles.carouselImag}/>
             </button>
         )) : <h1>Loading.....</h1>
         }
